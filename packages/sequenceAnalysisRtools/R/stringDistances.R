bcbae6070f2536b65f4079a52482d070a2bffe28
Andrea Lauria <andrea.lauria104@gmail.com>
2019-11-29 18:54:41 +0100

    update stringDistances - calculate_string_distance

--- packages/sequenceAnalysisRtools/R/stringDistances.R
+++ packages/sequenceAnalysisRtools/R/stringDistances.R
@@ -23,23 +23,40 @@ get_all_substr <- function(x, n, sub.method)
   return(ss)
 }
 
-calculate_string_distance <- function(x, sub.method = 'all')
+calculate_string_distance <- function(s, sub.method = "all", split.by = "\\-", verbose = F)
 {
-  s <- unlist(strsplit(x, "-"))
-  ln <- sapply(s, nchar)
-  
-  if(length(unique(ln))==1) {
-    message("[+] Computing Hamming distance")
-    d <- compute_hamming_distance(s)
-  } else {
-    message("[+] Unequal string lenghts, computing Hamming distance on substrings")
-    gidx <- which.max(ln)
-    lidx <- which.min(ln)
+  calc_distance <- function(s, sub.method, split.by, verbose) {
     
-    gs <- get_all_substr(x = s[gidx], n = ln[lidx], sub.method = sub.method)
-    d  <- c(gs, s[lidx])
-    d  <- combn(x = d, 2, FUN=compute_hamming_distance)
-    d  <- d[which.min(d)]
+    if( is.character(s) & length(s)==1 ) {
+      s <- unlist(strsplit(s, split.by))  
+    }
+    
+    ln <- sapply(s, nchar)
+    
+    if(length(unique(ln))==1) {
+      if(verbose) message("[+] Computing Hamming distance")
+      d <- compute_hamming_distance(s)
+    } else {
+      if(verbose) message("[+] Unequal string lenghts, computing Hamming distance on substrings")
+      gidx <- which.max(ln)
+      lidx <- which.min(ln)
+      
+      gs <- get_all_substr(x = s[gidx], n = ln[lidx], sub.method = sub.method)
+      d  <- c(gs, s[lidx])
+      d  <- combn(x = d, 2, FUN=compute_hamming_distance)
+      d  <- d[which.min(d)]
+    }
+    return(d)
   }
-  return(d)
+  
+  if(is.list(s)) s <- unique(unlist(s))
+  
+  if(length(s) <= 2) {
+    index_distance <- calc_distance(s = s, sub.method = sub.method, split.by = split.by, verbose = verbose)
+  } else {
+    index_comb <- combn(x = index_comb,2, FUN=paste, collapse = "-")
+    index_distance <- lapply(index_comb, calculate_string_distance, sub.method = sub.method, split.by = split.by, verbose = verbose)
+    names(index_distance) <- index_comb
+  }
+  return(index_distance)
 }
