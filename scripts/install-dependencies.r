be193c59fe3fecab4a2fa95d287883149a22476d
Andrea Lauria <andrea.lauria104@gmail.com>
2020-05-05 12:43:40 +0200

    update install-dep script

--- scripts/install-dependencies.r
+++ scripts/install-dependencies.r
@@ -11,12 +11,16 @@ CRAN <- "https://cran.rstudio.com/"
 cran_pkgs <- c( 'xlsx', 'ggplot2','ggrepel','reshape2', 'RColorBrewer',
                 'circlize', 'plyr', 'ggpubr','ggsci','grid','scales',
                 'VennDiagram','Rtsne','rjson','cluster','vegan',
-                'dynamicTreeCut', 'gProfileR')
+                'dynamicTreeCut', 'gProfileR',"DT")
 
 cran_not_installed <- cran_pkgs[which(!cran_pkgs %in% rownames(installed.packages()))]
 
-if(length(cran_not_installed)>0) lapply(cran_not_installed, install.packages, repos = CRAN)
-
+if(length(cran_not_installed)>0) {
+   for(i in cran_not_installed) {
+     install.packages(i, repos=CRAN)	
+ }
+}
+rm(i)
 # 2. Bioconductor ----
 if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager", repos = CRAN)
 
@@ -27,7 +31,11 @@ bioconductor_pkgs <- c('ComplexHeatmap','Biostrings','GenomicRanges','genomation
 
 bioconductor_not_installed <- bioconductor_pkgs[which(!bioconductor_pkgs %in% rownames(installed.packages()))]
 
-if(length(bioconductor_not_installed)>0) lapply(bioconductor_not_installed, BiocManager::install)
+if(length(bioconductor_not_installed)>0) {
+   for(i in bioconductor_not_installed) {
+     BiocManager::install(i)
+ }
+}
 
 # 3. Check installed packages ----
 cran_not_installed <- cran_pkgs[which(!cran_pkgs %in% rownames(installed.packages()))]
